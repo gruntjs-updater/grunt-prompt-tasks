f508760bbd692112045b1b3b2f7dc741d06d2528
matt <matt@clover.com>
2014-05-13 21:21:12 -0700

    changes

--- tasks/prompt_tasks.js
+++ tasks/prompt_tasks.js
@@ -9,6 +9,18 @@
 'use strict';
 
 module.exports = function(grunt) {
+    grunt.registerPromptTask = function (name, desc, tasks) {
+        if (arguments.length < 3) {
+            // desc was not specified, so let's swap tasks and desc
+            tasks = desc;
+            desc = null;
+        }
+
+        desc = desc || '';
+
+        grunt.writeln('registered task:' + name);
+    };
+
     grunt.registerTask('prompt_tasks', 'Prompt the user with a list of tasks to choose from.', function() {
         grunt.log.writeln('hello world');
     });
